92960ef8250652e7d950bbf874648464d2a10e7e
Sturla-ingason <Sturladi@gmail.com>
2026-04-02 15:53:30 +0000

    log Inn og sign Upp
    hægt núna að gera log inn og sign upp tengt við api

--- src/app/logInn/page.tsx
+++ src/app/logInn/page.tsx
@@ -1,4 +1,34 @@
+'use client'
 
-export default function logInn(){
-    return <p>Log inn page</p>
+import { useState, BaseSyntheticEvent } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function LogInn(){
+    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const router = useRouter()
+
+    async function handleSubmit(e: BaseSyntheticEvent) {
+        e.preventDefault()
+        const res = await fetch('http://localhost:3002/user/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+        })
+
+        const data = await res.json()
+        localStorage.setItem('token', data.token)
+        console.log(localStorage.getItem('token'))
+        router.push('/')
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input value={email} onChange={e => setEmail(e.target.value)} type='email'/>
+            <input value={password} onChange={e => setPassword(e.target.value)} type='password'/>
+            <button type="submit">Log Inn</button>
+        </form>
+    )
+
 }
--- src/app/signUpp/page.tsx
+++ src/app/signUpp/page.tsx
@@ -1,4 +1,35 @@
+'use client'
 
-export default function signUpp(){
-    return <p>Sign upp page</p>
+import { useState, BaseSyntheticEvent } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function SignUpp(){
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('')
+    const router = useRouter()
+
+    async function handleSignUpp(e: BaseSyntheticEvent){
+        e.preventDefault()
+        const res = await fetch('http://localhost:3002/user/createnewuser', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password, name }),
+        })
+
+        const data = await res.json()
+        localStorage.setItem('token', data.token)
+        router.push('/')
+    }
+
+    return (
+        <form onSubmit={handleSignUpp}>
+            <input value={email} onChange={e => setEmail(e.target.value)} type='email'/>
+            <input value={password} onChange={e => setPassword(e.target.value)} type='password'/>
+            <input value={name} onChange={e => setName(e.target.value)} type='name'/>
+            <button type='submit'>Sign upp</button>
+        </form>
+    )
+    
 }
